3b24a6a3ce75741cbf2992be3b65edf3c16a04ec
SoYoung210 <ethdud1@gmail.com>
2020-09-09 23:04:49 +0900

    ci: 🎡 add storybook dependency

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,30 @@
+const path = require('path');
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+
+async function yarn2Config(config, options) {
+  const newConfig = {
+    ...(config || {}),
+    resolve: {
+      ...((config || {}).resolve || {}),
+      plugins: [
+        ...(((config || {}).resolve || {}).plugins || []),
+        PnpWebpackPlugin
+      ]
+    },
+    resolveLoader: {
+      ...((config || {}).resolveLoader || {}),
+      plugins: [
+        ...(((config || {}).resolveLoader || {}).plugins || []),
+        PnpWebpackPlugin.moduleLoader(module)
+      ]
+    }
+  };
+  const jsRule = newConfig.module.rules.find(rule => rule.test.test(".js"));
+  jsRule.exclude = /node_modules/;
+
+  return newConfig;
+}
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -6,5 +33,6 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  managerWebpack: yarn2Config, webpack: yarn2Config
 }
