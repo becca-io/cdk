a415d0aa174c444b6bf866b7faf703a4fd25ddb1
SoYoung210 <ethdud1@gmail.com>
2020-09-09 23:32:50 +0900

    style: 💄 remove useless code

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,24 +2,22 @@ const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 async function yarn2Config(config, options) {
   const newConfig = {
-    ...(config || {}),
+    ...config,
     resolve: {
-      ...((config || {}).resolve || {}),
+      ...config.resolve,
       plugins: [
-        ...(((config || {}).resolve || {}).plugins || []),
+        ...config.resolve.plugins || [],
         PnpWebpackPlugin
       ]
     },
     resolveLoader: {
-      ...((config || {}).resolveLoader || {}),
+      ...(config.resolveLoader),
       plugins: [
-        ...(((config || {}).resolveLoader || {}).plugins || []),
+        ...config.resolveLoader.plugins,
         PnpWebpackPlugin.moduleLoader(module)
       ]
     }
   };
-  const jsRule = newConfig.module.rules.find(rule => rule.test.test(".js"));
-  jsRule.exclude = /node_modules/;
 
   return newConfig;
 }
