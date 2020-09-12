bbea75ac7b1971803413b0202cdbd596cce57af7
JaeYeopHan <ljyhanll@gmail.com>
2020-09-12 16:26:44 +0900

    fix: 🐛 update template generator

--- scripts/generate-new-kit.js
+++ scripts/generate-new-kit.js
@@ -1,24 +1,33 @@
-const inquirer = require('inquirer');
-const sh = require('shelljs');
+const inquirer = require("inquirer");
+const sh = require("shelljs");
 
 (async function command() {
   try {
     const { type, name, description } = await inquirer.prompt([
-      { type: 'list', name: 'type', message: 'Project Type', choices: ['package', 'example'] },
-      { type: 'string', name: 'name', message: 'Project Name' },
-      { type: 'string', name: 'description', message: 'Project Description' },
+      {
+        type: "list",
+        name: "type",
+        message: "Project Type",
+        choices: ["package", "example"],
+      },
+      { type: "string", name: "name", message: "Project Name" },
+      { type: "string", name: "description", message: "Project Description" },
     ]);
 
-    if (type === 'example') {
-      console.log('Not supported type');
+    if (type === "example") {
+      console.log("Not supported type");
 
       return;
     }
-    const packageName = `@becca/${name}`;
 
-    sh.exec(`npx hygen ${type} new --name ${packageName} --description ${description}`);
+    const path = `@becca-${name}`;
+    const packageName = `@becca/${name}`;
+    const packageDescription = description.split(" ").join("_");
+
+    sh.exec(
+      `npx hygen ${type} new --name ${packageName} --path ${path} --description ${packageDescription}`
+    );
   } catch (e) {
     console.error(e);
   }
-
 })();
